6cca52f1a55a4193319121c5645e700d6d595823
oohana2 <oohana2@naver.com>
2020-03-05 19:13:31 +0900

    .

--- chatting_class.cpp
+++ chatting_class.cpp
@@ -10,12 +10,12 @@ using namespace std;
 class chatting_room
 {
 private:
-    int my_id;
-    string chatting_room_id;
+    int my_id;               //본인 id
+    string chatting_room_id; //채팅방 고유 id
 
 public:
-    int read_time;                //어디까지 읽었는데 확인 20200305248 년도월날짜시간 순
-    char chatting_room_name[100]; //채팅방 이름 100자 제한
+    int read_time;             //어디까지 읽었는데 확인 20200305248 년도월날짜시간 순 기능 미구현
+    string chatting_room_name; //채팅방 이름 100자 제한
     string chatting_room_member[100];
 
     chatting_room(int _my_id, int _chatting_room_id)
@@ -52,6 +52,11 @@ string get_chatting_room_name()
     string tmp;
 
     getline(cin, tmp);
+
+    if (tmp[99] != NULL) // 100자 제한
+    {
+        get_chatting_room_name();
+    }
 
     return tmp;
 }
