95df26bd92c2454b78faa17719ed0559c5d5a7d0
정해성 <12171853@inha.edu>
2020-03-05 18:12:34 +0900

    모임전 Final

--- Server.h
+++ Server.h
@@ -3,10 +3,26 @@
 
 #define PORT 4578
 #define PACKET_SIZE 1024
-#define SERVER_IP "192.168.219.101" //해성 PC IP
+#define SERVER_IP "192.168.219.101" //해성 PC IP, 웹서버로 넣을시엔..?
 
 class Server
 {
 private:
 public:
+    void DB_init();      //연결
+    void DB_send();      //쿼리
+    void awaiting();     //변경사항(메세지 발신, 정보수정, 공지) 대기
+    void Notification(); //알림발송
+    void serverSocket(); //
+    void clientSocket();
+
+    Server();  //DB, 소켓 연결
+    ~Server(); //DB, 소켓 해제
 };
+
+/*
+데이터 베이스 함수
+1. DB접속 코드
+2. 쿼리 전달
+3. 알림 확인 후 전송
+*/
--- ServerMain.cpp
+++ ServerMain.cpp
@@ -1,61 +1,30 @@
 //20.3.3 21:00 정해성 최종 수정
 #include <iostream>
-#include <WinSock2.h> //소켓 사용을 위한 헤더파일
 #include "Server.h"
-
-#pragma comment(lib, "ws2_32.lib") //위에서 선언한 헤더파일들을 가져다 쓰기 위한 링크
 
 int main()
 {
-    //서버 측 코드
-    //Windows의 소켓 초기화 정보를 저장하기위한 구조체 선언
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    Server host;
+    Server client;
+    int a;
 
-    //소켓 생성
-    SOCKET hListen;
-    hListen = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); //(IPV4,연결지향성,TCP)
+    std::cout << "서버면 1, 클라이언트면 2를 누르세요." << std::endl;
+    std::cin >> a;
 
-    if (hListen == INVALID_SOCKET)
-        std::cout << "서버 생성 실패" << std::endl;
-
-    //소켓의 구성요소를 담을 구조체 생성 및 값 할당
-    SOCKADDR_IN tListenAddr = {};
-    tListenAddr.sin_family = AF_INET;
-    tListenAddr.sin_port = htons(PORT);              //이걸 거치면 빅엔디안 방식으로 데이터 설정
-    tListenAddr.sin_addr.s_addr = htonl(INADDR_ANY); //현재 동작되는 IP주소로 설정
-
-    //소켓에 위에 설정한 주소정보를 묶어줌
-    bind(hListen, (SOCKADDR *)&tListenAddr, sizeof(tListenAddr));
-    if (bind(hListen, (SOCKADDR *)&tListenAddr, sizeof(tListenAddr)) == SOCKET_ERROR)
-        std::cout << "바인딩 실패" << std::endl;
-
-    //소켓을 수신상태로 변경, 최대 접속승인수로 설정
-    listen(hListen, SOMAXCONN);
-
-    if (listen(hListen, SOMAXCONN) == SOCKET_ERROR)
-        std::cout << "접속대기 모드 실패" << std::endl;
+    if (a == 1)
+    {
+        std::cout << "서버 모드 확인" << std::endl;
+        host.serverSocket();
+    }
+    else if (a == 2)
+    {
+        std::cout << "클라이언트 모드 확인" << std::endl;
+        client.clientSocket();
+    }
     else
-        std ::cout << "클라이언트를 기다립니다.." << std::endl;
-
-    //클라이언트 소켓 생성, 정보구조체 생성 및 값 할당
-    SOCKADDR_IN tClntAddr = {};
-    int iClntSize = sizeof(tClntAddr);
-    SOCKET hClient = accept(hListen, (SOCKADDR *)&tClntAddr, &iClntSize); // 동기화된 방식으로 접속요청 수락
-    //(소켓, accept할 client주소 구조체의 주소, 2인자 크기 저장된 변수의 주소)
-
-    //clint 정보 출력
-    char cBuffer[PACKET_SIZE] = {};
-    recv(hClient, cBuffer, PACKET_SIZE, 0); //대상 소켓의 정보 받는 함수
-    std::cout << "Recv Msg : " << cBuffer << std::endl;
-
-    char cMsg[] = "Server Send";
-    send(hClient, cMsg, strlen(cMsg), 0);
-
-    closesocket(hClient);
-    closesocket(hListen);
-
-    WSACleanup();
-
+    {
+        std::cout << "서버(1), 클라이언트(2) 중에 선택하세요." << std::endl;
+        std::cin >> a;
+    }
     return 0;
 }
